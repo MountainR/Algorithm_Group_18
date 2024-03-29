a98698e4e67494d07d55d83e380782aa2b7287ce
rui <112938973+MountainR@users.noreply.github.com>
2024-03-29 14:33:11 +0000

    Non Recursive Merge Sort: initially completed and passes two simple tests in main

--- NonRecursiveMergeSort.java
+++ NonRecursiveMergeSort.java
@@ -18,17 +18,49 @@ public class NonRecursiveMergeSort {
      * @param end ending index of subarray 2
      */
     public static void merge(int[] array, int start, int middle, int end) {
+        // -------------------------- Preparation ---------------------------------
         // get two subarrays
-        int[] left_array = Arrays.copyOfRange(array, start, middle-1);
-        int[] right_array = Arrays.copyOfRange(array, middle, end);
+        int[] left_array = Arrays.copyOfRange(array, start, middle);
+        int[] right_array = Arrays.copyOfRange(array, middle, end+1);
 
         // pointers to the beginning of subarrays
         int left_index = 0;                  // start of subarray 1
         int right_index = 0;                  // start of subarray 2
-        // a pointer to the original array
-        int index = start;
 
-        // merge arrays
+        // a pointer to the original array
+        int origin_index = start;
+
+        // --------------------------- Merge Arrays --------------------------------
+
+        // 1. both left and right array are not be fully iterated
+        while(left_index < left_array.length && right_index < right_array.length) {
+            int left = left_array[left_index];
+            int right = right_array[right_index];
+            // if left < right, give left to original array. Update left index and origin index
+            if (left < right) {
+                array[origin_index] = left;
+                ++left_index;
+            }
+            // else, give right to original array. Update right index and origin index
+            else {
+                array[origin_index] = right;
+                ++right_index;
+            }
+            ++origin_index;
+        }
+
+        // 2. while left array is not fully iterated, give all of its rest elements to original array
+        while(left_index < left_array.length) {
+            array[origin_index] = left_array[left_index];
+            ++left_index;
+            ++origin_index;
+        }
+        // 3. while right array is not fully iterated, give all of its rest elements to original array
+        while(right_index < right_array.length) {
+            array[origin_index] = right_array[right_index];
+            ++right_index;
+            ++origin_index;
+        }
 
 
 
@@ -41,9 +73,13 @@ public class NonRecursiveMergeSort {
         System.out.println(Arrays.toString(arr)); // print 2
         */
 
-        int[] array = {1, 3, 5, 2, 4, 6};
-        merge(array, 0, 3, 5);
-        System.out.println(Arrays.toString(array));
+        int[] array_1 = {1, 3, 5, 2, 4, 6};
+        merge(array_1, 0, 3, 5);
+        System.out.println(Arrays.toString(array_1));
+
+        int[] array_2 = {0};
+        merge(array_2, 0, 0, 0);
+        System.out.println(Arrays.toString(array_2));
 
 
     }
