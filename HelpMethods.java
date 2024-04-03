ae56ad65880d6f1a9546a61f794c6bf152ef0a80
jultrishyyy <158044755+jultrishyyy@users.noreply.github.com>
2024-04-03 20:38:37 +0100

    HelpMethods.java

---
+++ HelpMethods.java
@@ -0,0 +1,58 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class HelpMethods {
+    public static int[] generateDataset(int length) {
+        int[] dataset = new int[length];
+
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            dataset[i] = random.nextInt(20001) - 10000;
+        }
+
+        return dataset;
+    }
+
+    public static boolean verifyCorrectness(int[] originalArray, int[] sortedArray) {
+        // Check if both arrays have the same length
+        if (originalArray.length != sortedArray.length) {
+            return false;
+        }
+
+        // Check if the sorted array is in non-decreasing order
+        for (int i = 0; i < sortedArray.length - 1; i++) {
+            if (sortedArray[i] > sortedArray[i + 1]) {
+                return false;
+            }
+        }
+
+        // Count occurrences in both arrays
+        Map<Integer, Integer> originalCounts = new HashMap<>();
+        Map<Integer, Integer> sortedCounts = new HashMap<>();
+
+        for (int i = 0; i < originalArray.length; i++) {
+            originalCounts.put(originalArray[i], originalCounts.getOrDefault(originalArray[i], 0) + 1);
+            sortedCounts.put(sortedArray[i], sortedCounts.getOrDefault(sortedArray[i], 0) + 1);
+        }
+
+        // Compare occurrences of each element in both arrays
+        for (int number : originalCounts.keySet()) {
+            if (!originalCounts.get(number).equals(sortedCounts.get(number))) {
+                return false;
+            }
+        }
+
+        // If all checks pass, the array is correctly sorted
+        return true;
+    }
+
+    public static void printDataset(int[] dataset) {
+        int length = Math.min(dataset.length, 15);
+        for (int i = 0; i < length; i++) {
+            System.out.println(dataset[i]);
+        }
+        System.out.println("\n");
+    }
+}
