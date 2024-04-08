99e11c52c5dd39e9abb35e29a5bd5d2c4cf25386
jultrishyyy <158044755+jultrishyyy@users.noreply.github.com>
2024-04-08 22:10:23 +0100

    Main.java

--- Main.java
+++ Main.java
@@ -12,11 +12,23 @@ public class Main {
         dataset3 = HelpMethods.generateDataset(5000000);
 
         // Call MSQSA and POSA with the all three datasets as input.
-
+        System.out.println("--------------------Dataset with 1000000 integers--------------------");
         MSQSA.sort(dataset1,1);
         MSQSA.sort(dataset1,2);
         MSQSA.sort(dataset1,4);
         MSQSA.sort(dataset1,8);
+
+        System.out.println("\n\n--------------------Dataset with 2000000 integers--------------------");
+        MSQSA.sort(dataset2,1);
+        MSQSA.sort(dataset2,2);
+        MSQSA.sort(dataset2,4);
+        MSQSA.sort(dataset2,8);
+
+        System.out.println("\n\n--------------------Dataset with 5000000 integers--------------------");
+        MSQSA.sort(dataset3,1);
+        MSQSA.sort(dataset3,2);
+        MSQSA.sort(dataset3,4);
+        MSQSA.sort(dataset3,8);
 
 
 //        HelpMethods.printDataset(dataset1);
