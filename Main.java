2a02f26b6a833ba6d218555dbcdd2bbb283af867
jultrishyyy <158044755+jultrishyyy@users.noreply.github.com>
2024-04-18 09:17:09 +0100

    Main.java

--- Main.java
+++ Main.java
@@ -7,15 +7,13 @@ import java.util.Random;
 public class Main {
     private static int[] dataset1, dataset2, dataset3;
     public static void main(String[] args) {
-//        dataset1 = HelpMethods.generateDataset(1000000);
-//        dataset2 = HelpMethods.generateDataset(2000000);
-//        dataset3 = HelpMethods.generateDataset(5000000);
 
-        // Call MSQSA and POSA with the thread count as input 
+        // Call MSQSA and POSA with the thread count as input
         // and an ArrayList containing the running time for all 3 datasets
-        MSQSA.sort(1);
-        MSQSA.sort(2);
-        MSQSA.sort(4);
-        MSQSA.sort(8);
+        System.out.println("--------------------Dataset size: 1000000--------------------");
+        MSQSA.sort(1000000,1);
+        MSQSA.sort(1000000,2);
+        MSQSA.sort(1000000,4);
+        MSQSA.sort(1000000,8);
     }
 }
