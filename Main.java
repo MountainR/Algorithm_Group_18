0fd3cb8df6efa07ccfa02f41e66369dbaf1d9a34
rui <112938973+MountainR@users.noreply.github.com>
2024-04-18 10:19:13 +0100

    add tests in MSQSA

--- Main.java
+++ Main.java
@@ -8,12 +8,26 @@ public class Main {
     private static int[] dataset1, dataset2, dataset3;
     public static void main(String[] args) {
 
-        // Call MSQSA and POSA with the thread count as input
+        // Call MSQSA with the thread count as input
         // and an ArrayList containing the running time for all 3 datasets
         System.out.println("--------------------Dataset size: 1000000--------------------");
         MSQSA.sort(1000000,1);
         MSQSA.sort(1000000,2);
         MSQSA.sort(1000000,4);
         MSQSA.sort(1000000,8);
+
+        // and an ArrayList containing the running time for all 3 datasets
+        System.out.println("--------------------Dataset size: 2000000--------------------");
+        MSQSA.sort(2000000,1);
+        MSQSA.sort(2000000,2);
+        MSQSA.sort(2000000,4);
+        MSQSA.sort(2000000,8);
+
+        // and an ArrayList containing the running time for all 3 datasets
+        System.out.println("--------------------Dataset size: 5000000--------------------");
+        MSQSA.sort(5000000,1);
+        MSQSA.sort(5000000,2);
+        MSQSA.sort(5000000,4);
+        MSQSA.sort(5000000,8);
     }
 }
